ceab31b5a8b28998db9e09d8f38a43cdfd410af5
Bas Wilson <friedcatps@gmail.com>
2022-08-17 23:48:22 +0200

    absolute path

--- cmd/block_init/main.go
+++ cmd/block_init/main.go
@@ -2,18 +2,26 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"path"
+	"strings"
 
 	block "github.com/baswilson/block/internal/block"
 	"github.com/baswilson/block/internal/webserver"
 	"github.com/joho/godotenv"
 )
 
+var Base string
+
 func main() {
 	block.ValidateOS()
 	
 	fmt.Println("[BLOCK] initializing block")
-	
-	godotenv.Load("build/ci/dev/sim.env")
+
+	executable, _ := os.Executable();
+	Base := strings.Split(executable, "/bin/block_init")[0]
+
+	godotenv.Load(path.Join(Base, "build/ci/dev/sim.env"))
 
 	// Start up webserver
 	webserver.SetupRouter()
