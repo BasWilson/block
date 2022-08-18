825a8d346861a84410f212c85dd726fbabc50a79
Bas Wilson <friedcatps@gmail.com>
2022-08-18 16:15:21 +0200

    env variables are now hidden

--- internal/block/webserver/routes/containers.go
+++ internal/block/webserver/routes/containers.go
@@ -7,7 +7,17 @@ import (
 
 func Containers(c *gin.Context) {
 
+	// create empty slice of containers
+	containers := make([]block.Config, len(block.Configs))
+	
+	for i := 0; i < len(containers); i++ {
+		container := block.Configs[i]
+		for x := 0; x < len(container.Variables); x++ {
+			container.Variables[x].Value = "*************";
+		}
+	}
+
 	c.JSON(200, gin.H{
-		"containers": block.Configs,
+		"containers": containers,
 	})
 }
