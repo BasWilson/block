ea0acffc65eb4bdabf4757821f9653babdcc65bf
Bas Wilson <friedcatps@gmail.com>
2023-01-25 06:51:39 +0100

    new scripts

---
+++ scripts/create_ssl_cert.sh
@@ -0,0 +1,5 @@
+# Generate a self-signed SSL certificate for the server with letsencrypt
+# Usage: ./create_ssl_cert.sh
+#
+# This script is based on the following tutorial:
+# https://www.digitalocean.com/community/tutorials/how-to-secure-nginx-with-let-s-encrypt-on-ubuntu-20-04
---
+++ scripts/setup_nginx.sh
@@ -0,0 +1,5 @@
+# Setup Nginx on Ubuntu 20.04
+# Usage: ./setup_nginx.sh
+#
+# This script is based on the following tutorial:
+# https://www.digitalocean.com/community/tutorials/how-to-install-nginx-on-ubuntu-20-04
